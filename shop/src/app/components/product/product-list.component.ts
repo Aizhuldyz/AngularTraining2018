73cd6275e540ead2959d76d8612d9bbf0bdb1998
Aizhuldyz Gabbassova <Aizhuldyz_Gabbassova@epam.com>
2018-01-14 21:06:46 +0600

    product list filter with formsmodule

--- shop/src/app/components/product/product-list.component.ts
+++ shop/src/app/components/product/product-list.component.ts
@@ -8,12 +8,30 @@ import { ProductService } from './product.service';
     templateUrl: './product-list.component.html'
 })
 export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List';
+    pageTitle = 'Product List';
     products: Product[];
+    filteredProducts: Product[];
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
 
     constructor ( private _productService: ProductService) {}
 
+    performFilter(listFilter: string): Product[] {
+        listFilter = listFilter.toLocaleLowerCase();
+        return this.products.filter((product: Product) =>
+        product.name.toLocaleLowerCase().indexOf(listFilter) !== -1);
+    }
+
     ngOnInit() {
         this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
