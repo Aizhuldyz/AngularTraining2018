6e89377edbab930634b0939804c52c25c04c04f9
aizhuldyz gabbassova <aigabbass90@gmail.com>
2018-02-01 18:44:37 +0600

    logininfo model

---
+++ shop/src/app/services/models/login-info.model.ts
@@ -0,0 +1,11 @@
+export class LoginInfo {
+    constructor(
+        public id: number,
+        public login: string,
+        public email: string,
+    ) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+    }
+}
